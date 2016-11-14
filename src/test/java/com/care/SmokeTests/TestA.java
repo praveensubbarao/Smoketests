2be611fd7fbe4ede4685e301b79bf42a9b3c0418
Praveen Subbarao <psubbarao@Wal-Psubbarao2.local>
2016-11-14 17:59:24 -0500

    first commit

---
+++ src/test/java/com/care/SmokeTests/TestA.java
@@ -0,0 +1,23 @@
+package com.care.SmokeTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestA {
+
+	@Test()
+	public void PassTest() {
+
+		Assert.assertEquals("qa@test.com", "qa@test.com");
+
+	}
+	
+	@Test()
+	public void FailTest() {
+
+		Assert.assertEquals("qa1@test.com", "qa@test.com");
+
+	}
+
+}
+
